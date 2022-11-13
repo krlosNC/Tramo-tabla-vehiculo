95d7eabf562002106804bd0ea8e41779977cc20e
krlosNC <krlosnoguera21@gmail.com>
2022-11-13 16:25:30 -0500

    TABLA VEHICULO TRAMO

---
+++ js/main.js
@@ -0,0 +1,55 @@
+//__________________________________________________________________________//
+//____________________________   GET    ____________________________________//
+//__________________________________________________________________________//
+
+class Flota {
+
+  vehiculo = {
+    _tipoVehiculo: "",
+    _marcaVehiculo: "",
+    _modeloVehiculo: "",
+    _numeroEjesVehiculo: 0,
+    _tipoTraccionVehiculo: "",
+    _placaVehiculo: "",
+    _placaTrailerVehiculo: "",
+    _pesoVacioVehiculo: 0,
+    _tipoCombustibleVehiculo: "",
+    _numeroLicenciaVehiculo: "",
+    _numeroSoatVehiculo: "",
+    _fechaVencimientoSoat: 0,
+    _numeroPolizaResponsabilidad: 0,
+    _numeroTecnomecanicaVehiculo: 0,
+    _fechaVencimientoTecnomecanica: 0
+  };
+
+  agregarVehiculo(tipo, marca, modelo, numEjes, traccion, placa, placaSR, pesoVH, combustible, licenciaVH, soatVH, venciSoat, polizaRes, tecnoVH, venciTecno) {
+
+    this.vehiculo._tipoVehiculo = tipo;
+    this.vehiculo._marcaVehiculo = marca;
+    this.vehiculo._modeloVehiculo = modelo;
+    this.vehiculo._numeroEjesVehiculo = numEjes;
+    this.vehiculo._tipoTraccionVehiculo = traccion;
+    this.vehiculo._placaVehiculo = placa;
+    this.vehiculo._placaTrailerVehiculo = placaSR;
+    this.vehiculo._pesoVacioVehiculo = pesoVH;
+    this.vehiculo._tipoCombustibleVehiculo = combustible;
+    this.vehiculo._numeroLicenciaVehiculo = licenciaVH;
+    this.vehiculo._numeroSoatVehiculo = soatVH;
+    this.vehiculo._fechaVencimientoSoat = venciSoat;
+    this.vehiculo._numeroPolizaResponsabilidad = polizaRes;
+    this.vehiculo._numeroTecnomecanicaVehiculo = tecnoVH;
+    this.vehiculo._fechaVencimientoTecnomecanica = venciTecno;
+
+    console.table(this.getVehiculo);
+  
+  }
+
+  get getVehiculo(){
+    return this.vehiculo
+  }
+
+}
+
+var instmisObjetos = new Flota();
+
+
